608335ad4497754d0a19805e911fe054008fbf7e
Tivakorn <not_not_not1@hotmail.co.th>
2021-09-05 04:09:43 +0700

    :test_tube: test push code on github

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 // Routes
 import Post from './post'
 import Member from './member'
+import Blog from './blog'
 
-export type Routers = (typeof Post | typeof Member)
+export type Routers = (typeof Post | typeof Member | typeof Blog)
 
 export const routers: Routers[] = [
     Post,
-    Member
+    Member,
+    Blog
 ]
